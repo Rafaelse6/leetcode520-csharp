cffe6d046b302b0b641643fc6537a57068fe7d4b
Rafael Santos <64181619+Rafaelse6@users.noreply.github.com>
2025-02-08 22:50:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution
+{
+    public bool DetectCapitalUse(string word)
+    {
+        if (word == word.ToUpper())
+        {
+            return true;
+        }
+        if (word == word.ToLower())
+        {
+            return true;
+        }
+        if (char.IsUpper(word[0]) && word.Substring(1) == word.Substring(1).ToLower())
+        {
+            return true;
+        }
+        return false;
+    }
+}
